36844824194525292e8575fa7b2e3f4baeb4a60d
Pauline Kaoomi <pauav22015@gmail.com>
2023-12-03 13:46:52 -0500

    import component y cambio de ico

---
+++ carpeta-angular/airbeat-front/src/app/components/musicplayer/musicplayer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-musicplayer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './musicplayer.component.html',
+  styleUrl: './musicplayer.component.css'
+})
+export class MusicplayerComponent {
+
+}
