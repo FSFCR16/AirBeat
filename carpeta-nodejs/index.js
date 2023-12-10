0578c68b8c564793967bc20d404b93a73a5c5aa7
FSFCR16 <fajarfsabo161@gmail.com>
2023-12-09 22:13:31 -0500

    update nodejs

--- carpeta-nodejs/index.js
+++ carpeta-nodejs/index.js
@@ -1,7 +1,9 @@
 import express from 'express'; // se llama express que es un framework de node para crear servidores web
 import connectToDataBase from './Config/db.js';
 import dotenv from 'dotenv';  // se llama dotenv que es un modulo que permite usar variables de entorno
-import songsrouter from './Routes/routes.js'; // se llama el archivo de rutas y se importan las funciones
+import songsrouter from './Routes/routes.canciones.js'; // se llama el archivo de rutas y se importan las funciones
+import userRouter from './Routes/routes.user.js'
+
 
 dotenv.config();  // se llama la funcion config de dotenv
 const PORT= process.env.PORT;  // se crea una constante que guarda el puerto de la variable de entorno
@@ -10,6 +12,8 @@ connectToDataBase(); // se llama la funcion que conecta con la base de datos
 
 app.use(express.json());  // se usa la funcion json de express
 app.use("/songs", songsrouter);  // se usa la funcion use de express para que el servidor use las rutas del archivo de rutas")
+app.use("/user", userRouter )
+
 
 app.listen(PORT, () => {
   console.log('La aplicación de Airbeat está en ejecución en el puerto,',PORT);  // se usa la funcion listen de express para que el servidor escuche en el puerto 3000
