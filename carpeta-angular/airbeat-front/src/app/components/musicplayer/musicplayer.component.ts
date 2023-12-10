db15c3ecf5b9bdb99bcc578325a915a1c2828bc9
Pauline Kaoomi <pauav22015@gmail.com>
2023-12-10 00:01:49 -0500

    avance reproductor

--- carpeta-angular/airbeat-front/src/app/components/musicplayer/musicplayer.component.ts
+++ carpeta-angular/airbeat-front/src/app/components/musicplayer/musicplayer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as Howler from "howler";
 
 @Component({
   selector: 'app-musicplayer',
@@ -9,5 +10,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './musicplayer.component.css'
 })
 export class MusicplayerComponent {
+  @ViewChild ("btnPlay") btnPlay!:ElementRef
+  sound= new Howler.Howl({
+    src: ['../../assets/music.mp3']
+  })
 
+  reproducirCancion(){
+    this.sound.play()
+  }
 }
