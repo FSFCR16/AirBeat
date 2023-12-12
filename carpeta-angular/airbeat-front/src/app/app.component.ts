9cfbe2329ad1611892f4b82af6ffee9b1ae9f8e8
wilfridorg <wilfridorg@gmail.com>
2023-12-11 21:09:52 -0500

    anexo nabvar

--- carpeta-angular/airbeat-front/src/app/app.component.ts
+++ carpeta-angular/airbeat-front/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MusicplayerComponent } from './components/musicplayer/musicplayer.component'
+import { NabvarComponent } from './components/nabvar/nabvar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MusicplayerComponent],
+  imports: [CommonModule, RouterOutlet, MusicplayerComponent, NabvarComponent ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
