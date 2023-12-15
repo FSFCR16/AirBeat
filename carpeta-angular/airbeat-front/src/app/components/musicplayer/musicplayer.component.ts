f76a7062e471da2cde797c9569c35ea409d82cfc
Pauline Kaoomi <pauav22015@gmail.com>
2023-12-14 19:27:54 -0500

    updates reproductor

--- carpeta-angular/airbeat-front/src/app/components/musicplayer/musicplayer.component.ts
+++ carpeta-angular/airbeat-front/src/app/components/musicplayer/musicplayer.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Component,ViewChild,ElementRef, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as Howler from "howler";
 
@@ -11,16 +11,34 @@ import * as Howler from "howler";
 })
 export class MusicplayerComponent {
   @ViewChild ("btnPlay") btnPlay!:ElementRef
+  //@ViewChild ("duration") duration!:ElementRef
+   duracion:string | undefined;
+  public displayNone = false
   sound= new Howler.Howl({
-    src: ['../../assets/music.mp3']
+    src: ['../../assets/music.mp3'],
+    onplay: function(){
+      
+    }
   })
 
+  formatTime(secs:number) {
+    var minutes = Math.floor(secs / 60) || 0;
+    var seconds = (secs - minutes * 60) || 0;
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+  }
+
+  musica(){
+    this.sound.on("play",()=>{this.duracion=this.formatTime(Math.round(this.sound.duration()))})
+  }
+
   playMusic(){
-    this.sound.play()
-    
+    this.sound.play();
+    this.displayNone = !this.displayNone;
+    this.musica()
   }
 
   stopMusic(){
     this.sound.pause()
+    this.displayNone = !this.displayNone;
   }
 }
