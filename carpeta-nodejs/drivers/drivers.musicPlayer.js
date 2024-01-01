832597b71006195291c5b57c98f1c89a0cc66eaf
FSFCR16 <fajarfsabo161@gmail.com>
2023-12-31 22:49:13 -0500

    update

--- carpeta-nodejs/drivers/drivers.musicPlayer.js
+++ carpeta-nodejs/drivers/drivers.musicPlayer.js
@@ -1,28 +1,26 @@
 import { player } from "../models/musicPlayer.model.js";
 import { Music } from "../models/models.canciones.js";
-import { busquedaSchema } from "../models/busqueda.usuarios.models.js";
 
 export const play = async (req, res)=>{
     const _idUser= req.user._id
     const _idCancion= req.params._id
-    console.log(_idCancion)
 
 
     try{
         const song = await Music.findOne({_id: _idCancion})
-        const songHistorial= await busquedaSchema.findOne({cancionId: _idCancion})
         console.log(song)
-        if(!song || !songHistorial){
+
+        if(!song){
             return res.status(404).json({error: "No se encontro la cancion"})
         }
 
         const nuevaBusqueda= await player.create({
             userId: _idUser, 
             cancionId: _idCancion,
-            preview_url: song.preview_url || songHistorial.preview_url,
-            songName: song.name_track || songHistorial.songName,
-            songImage: song.img_urls || songHistorial.songImage,
-            songArtist: song.artist || songHistorial.songArtist
+            preview_url: song.preview_url,
+            songName: song.name_track,
+            songImage: song.img_urls,
+            songArtist: song.artist
         })
         await nuevaBusqueda.save();
         res.status(200).json({message: "Busqueda guardada con exito"})
@@ -31,4 +29,12 @@ export const play = async (req, res)=>{
         console.log(error)
         res.status(500).json({error:error})
     }
+}
+
+export const getsong = async(req, res)=>{
+    const idUser = req.user._id
+
+    const cancionPlayer=await player.findOne({userId:idUser})
+
+    
 }
