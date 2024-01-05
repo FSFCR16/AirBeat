fc78d0d27d1649735967517fba779d32439859b3
sebas1acero <sebas1acero@gmail.com>
2024-01-05 14:24:57 -0500

    modificacion 1

---
+++ carpeta-angular/airbeat-front/src/app/components/mainbeta/mainbeta.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mainbeta',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mainbeta.component.html',
+  styleUrl: './mainbeta.component.css'
+})
+export class MainbetaComponent {
+
+}
--- carpeta-angular/airbeat-front/src/app/pages/viewbeta/viewbeta.component.ts
+++ carpeta-angular/airbeat-front/src/app/pages/viewbeta/viewbeta.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{NavviewbetaComponent} from '../../components/navviewbeta/navviewbeta.component';
-
+import{ MainbetaComponent } from '../../components/mainbeta/mainbeta.component';
+import { AlbumComponent } from '../../components/album/album.component';
 
 @Component({
   selector: 'app-viewbeta',
   standalone: true,
-  imports: [CommonModule, NavviewbetaComponent],
+  imports: [CommonModule, NavviewbetaComponent, MainbetaComponent,AlbumComponent],
   templateUrl: './viewbeta.component.html',
   styleUrl: './viewbeta.component.css'
 })
