cdfc60fc8d4acbbb675695504827bd554ce446fc
sebas1acero <sebas1acero@gmail.com>
2024-01-05 15:09:32 -0500

    modificacion 2

--- carpeta-angular/airbeat-front/src/app/components/mainbeta/mainbeta.component.ts
+++ carpeta-angular/airbeat-front/src/app/components/mainbeta/mainbeta.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-mainbeta',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterOutlet, RouterModule],
+
   templateUrl: './mainbeta.component.html',
   styleUrl: './mainbeta.component.css'
 })
 export class MainbetaComponent {
+routeParameterValue: any;
 
 }
