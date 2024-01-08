07e7252691bc814dc3435141f80084e19c7beabc
Pauline Kaoomi <pauav22015@gmail.com>
2024-01-07 19:43:49 -0500

    home, botones redireccion

--- carpeta-angular/airbeat-front/src/app/components/vista-principal/vista-principal.component.ts
+++ carpeta-angular/airbeat-front/src/app/components/vista-principal/vista-principal.component.ts
@@ -1,13 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet,RouterLink } from '@angular/router';
+import { BucadorServiciosService } from '../../services/bucador.servicios.service';
+import { Howl, Howler } from "howler";
+import { songs } from '../../services/bucador.servicios.service';
+import { Router } from '@angular/router';
+import { busqueda } from '../../services/bucador.servicios.service';
+import { Subscription } from 'rxjs';
+import { usuarioService } from '../../services/vistaperfil.service';
+import { error } from 'console';
+
 @Component({
   selector: 'app-vista-principal',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RouterLink],
   templateUrl: './vista-principal.component.html',
   styleUrl: './vista-principal.component.css'
 })
-export class VistaPrincipalComponent {
+export class VistaPrincipalComponent implements OnInit {
+  @ViewChildren('btnEscuchar') btns!: QueryList<ElementRef>;
+  private busquedaSubscription: Subscription | undefined;
+  enlacesCanciones: string[] = [];
+  sound: Howl | undefined;
+  datos: songs[]= [];
+  secundarias :songs[] =[]
+  isFocused: boolean = false;
+  historial: busqueda[]=[]
+  usuario: any = {};
+  alert: boolean = false;
+  alerterror: boolean = false;
+  alertgeneral: boolean = false;
 
+  constructor(private usuarioService: usuarioService, private buscador: BucadorServiciosService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.obtenerUsuario();
+    this.buscador.traerHistorial().subscribe({
+      next:(data:any)=>{
+        this.historial = data.historial
+        console.log(this.historial)
+      },
+      error: (error)=>{
+        return error
+      }
+    })
+  }
+
+  obtenerUsuario(): void {
+    this.usuarioService.obtenerUsuario().subscribe(
+      (data) => {
+        this.usuario = data;
+        console.log(data)
+      },
+      (error) => {
+        if (error.status === 404) {
+          this.alertgeneral = true;
+          console.error('Usuario no encontrado 404:', error);
+        } else if (error.status === 500) {
+          this.alertgeneral = true;
+          console.error('Error del servidor 500:', error);
+        } else {
+          this.alertgeneral = true;
+          console.error('Error al obtener usuario:', error);
+        }
+      }
+    );
+  }
+
+  changeColorInput(status: boolean) {
+    this.isFocused = status;
+
+  }
+
+  cargarCancion(url: any): void {
+    this.buscador.guardarInformacion(url);
+  }
+
+  saveHistorial(idSong:string){
+    this.buscador.saveSongs(idSong).subscribe({
+      next: (data)=>{
+        console.log(data)
+      },
+      error:(error)=>{
+        console.log(error)
+      }
+    })
+  }
+
+  borrarDelHistorial(id:string){
+    this.buscador.borrarHistorial(id).subscribe({
+      next:(data)=>{
+        window.location.reload()
+        console.log("Eliminado correctamente: ", data)
+      },
+      error:(error)=>{
+        console.log(error)
+      }
+    })
+  }
+
+  ultimoClick(id:string){
+
+    this.buscador.guardarUltimaCancion(id).subscribe({
+      next:(data)=>{
+        console.log("Ultima cancion ", data )
+
+      },
+      error:(error)=>{
+        console.log(error)
+      }
+    })
+
+  }
+  
 }
+
+
