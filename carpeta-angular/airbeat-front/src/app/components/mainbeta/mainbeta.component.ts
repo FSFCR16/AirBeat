e711b484c5a524b55de1a384d844d5aa7a8674c4
FSFCR16 <fajarfsabo161@gmail.com>
2024-01-09 17:10:41 -0500

    update

--- carpeta-angular/airbeat-front/src/app/components/mainbeta/mainbeta.component.ts
+++ carpeta-angular/airbeat-front/src/app/components/mainbeta/mainbeta.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { BucadorServiciosService, songs } from '../../services/bucador.servicios.service';
 
 @Component({
   selector: 'app-mainbeta',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule],
 
   templateUrl: './mainbeta.component.html',
   styleUrl: './mainbeta.component.css'
 })
 export class MainbetaComponent {
-routeParameterValue: any;
+  routeParameterValue: any;
+  cancionesPredeterminadas: songs[] = []
+  constructor(private buscador: BucadorServiciosService) { }
 
+  ngOnInit() {
+    this.buscador.traerCancionAleatorias().subscribe({
+      next: (data: any) => {
+        this.cancionesPredeterminadas = data
+        console.log(data)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
 }
+
+
